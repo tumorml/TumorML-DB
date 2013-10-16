49c4195d7bc75eb6e48afe6fcae6e9993db1ccfa
David Johnson <djbytes@acm.org>
2013-10-16 23:35:11 +0100

    added BaseXClient; implemented simple search query by title using fuzzy full-text search

--- src/main/scala/org/tumorml/db/TumorDbServlet.scala
+++ src/main/scala/org/tumorml/db/TumorDbServlet.scala
@@ -1,12 +1,25 @@
 package org.tumorml.db
 
 import org.scalatra._
-import scalate.ScalateSupport
+import org.basex.client.api.BaseXClient
+import scala.collection.mutable.ListBuffer
+import scala.xml.{XML, Elem}
 
 class TumorDbServlet extends TumorDbStack {
 
   get("/search/:query") {
     contentType="application/xml"
+    val session = new BaseXClient("localhost", 1984, "admin", "admin")
+    val queryDocsByTitle = "db:open('tumorml')//tumorml[./header/title contains text '" + params({"query"}) +
+      "' using fuzzy]"
+    val docsByTitleResults = session.query(queryDocsByTitle)
+    val docsByTitleResultsList = new ListBuffer[Elem]
+    while (docsByTitleResults.more()) docsByTitleResultsList += XML.loadString(docsByTitleResults.next())
+    docsByTitleResults.close()
+    val output = <success>
+      {for (doc <- docsByTitleResultsList) yield doc}
+    </success>
+    output
   }
 
   get("/download/:id") {
